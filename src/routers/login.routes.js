7e28cd4372133bf5b90ac3f665858896aff47df7
Victor Salles <victor.salles@engenharia.ufjf.br>
2023-02-07 17:14:39 -0300

    Removido arquivo cópia do talker.json

--- src/routers/login.routes.js
+++ src/routers/login.routes.js
@@ -5,7 +5,7 @@ const router = express.Router();
 const generateToken = require('../utils/generateToken');
 const validateLogin = require('../middlewares/loginValidate');
 
-router.post('/', validateLogin, (req, res) => {
+router.post('/', validateLogin, (_req, res) => {
     res.status(200).json({ token: generateToken() });
   });
 
